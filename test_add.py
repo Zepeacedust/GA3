4eb4228901104869823c42d1e7ce3aa6f664a761
Zepeacedust <arnor0903@gmail.com>
2022-03-31 21:24:17 +0000

    Refactor: tók út smá infrastructure sem ég hélt að þurfti en þurfti ekki

--- test_add.py
+++ test_add.py
@@ -1,23 +1,25 @@
 import pytest
 from add import add
 
+## step 1 
 @pytest.fixture(autouse=True)
-def run_around_tests():
-    yield
+
 
 def test_empty_add():
     assert add("") == 0
 
+
 def test_single_number_a():
     assert add("1") == 1
-    
+
 
 def test_single_number_b():
     assert add("6") == 6
-    
-    
+
+
 def test_single_number_c():
     assert add("86") == 86
+
 
 def test_single_number_d():
     assert add("0") == 0
